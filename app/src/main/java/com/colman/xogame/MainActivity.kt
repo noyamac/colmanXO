d82870fbe07a002a82aa50dbec6336063faf62c4
noya <noyam1003@gmail.com>
2026-01-26 18:28:04 +0200

    init grid activity

--- app/src/main/java/com/colman/xogame/MainActivity.kt
+++ app/src/main/java/com/colman/xogame/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.colman.xogame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.colman.xogame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private val board = arrayOf("", "", "", "", "", "", "", "", "")
+    private lateinit var buttons: Array<Button?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        buttons = arrayOf(
+            binding?.button0, binding?.button1, binding?.button2,
+            binding?.button3, binding?.button4, binding?.button5,
+            binding?.button6, binding?.button7, binding?.button8
+        )
+
+        buttons.forEachIndexed { index, button ->
+            button?.setOnClickListener { onCellClicked(button, index) }
+            button?.text = ""
         }
     }
+
+    private fun onCellClicked(button: Button, index: Int) {
+        //TODO: implement
+    }
+
 }
